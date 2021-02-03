dba8321cb3b8ad8a6e20d3deb11a680c5a0d3156
eric-rafael <eduardo.ontecnologia@ccp.caedu>
2021-02-03 17:51:40 -0300

    readme editado

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ export default function Index(){
     return (
         <div>
             <Header />
-            <p>Olá, estou aprendendo <a href="https://nextjs.org/">Next.js</a>. Obrigado por acessar meu site</p>
+            <p>Olá, estou aprendendo <a href="https://nextjs.org/" target="_blank">Next.js</a>. Obrigado por acessar meu site</p>
             <p>Por enquanto estou construindo um menu de navegação!</p>
-            <p>O repositório desta página está em <a href="https://github.com/eric-rafael/siteteste">github.com/eric-rafael/siteteste</a></p>
+            <p>O repositório desta página está em <a href="https://github.com/eric-rafael/siteteste" target="_blank">github.com/eric-rafael/siteteste</a></p>
             <p>clique em um dos nomes acima para acessar as páginas oficiais</p>
-            <p>Caso queira se comunicar comigo meu e-mail é <a href="mailto:ericrafael753@gmail.com?subject=Questions " >ericrafael753@gmail.com</a></p>
+            <p>Caso queira se comunicar comigo meu e-mail é <a href="mailto:ericrafael753@gmail.com?subject=Questions">ericrafael753@gmail.com</a></p>
         </div>
     )
 }
