0406f353b26fe1a2deab0741b96882b403e88d68
eric-rafael <eduardo.ontecnologia@ccp.caedu>
2021-02-03 17:11:41 -0300

    primeiro site

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Index(){
             <Header />
             <p>Olá, estou aprendendo Next.js. Obrigado por acessar meu site</p>
             <p>Por enquanto estou construindo um menu de navegação!</p>
-            <p>O repositórrio desta página está em https://github.com/eric-rafael/siteteste</p>
+            <p>O repositório desta página está em https://github.com/eric-rafael/siteteste</p>
             <p>clique em um dos nomes acima para acessar as páginas oficiais</p>
             <p>Caso queira se comunicar comigo meu e-mail é ericrafael753@gmail.com.</p>
         </div>
